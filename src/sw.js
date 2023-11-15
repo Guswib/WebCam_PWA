12146f0501949c84a3af6e7ea49eb41e942cfdb5
Gustav Wiberg <gustav.wiberg@gmail.com>
2023-11-15 17:46:53 +0100

    aa

--- src/sw.js
+++ src/sw.js
@@ -12,42 +12,19 @@ var file_cashe = [
    
 ];
 
-var bilder_cashe = [
-    './bild/button_ramp.svg',
-    './bild/button_swv.svg',
-    './bild/button_steps.svg',
-    './bild/button_cell_cc.svg',
-    './bild/button_cell_oc.svg',
-    './bild/button_serial_on.svg',
-    './bild/button_serial_off.svg',
-    './bild/button_run.svg',
-    './bild/img_abort.svg',
-    './bild/img_ble.svg',
-    './bild/img_clear_plot.svg',
-    './bild/img_copy.svg',
-    './bild/img_save.svg',
-    './bild/img_serial.svg',
-    './bild/wButton_Graph_Rescale_Y.png',
-    './bild/img_dialog_yes.svg',
-    './bild/img_dialog_no.svg'];
 
 var icons_cashe = [
     './bild_ico/EC4_WebCam.svg',
-    'Nordic-Electrochemistry-Logo.png',
     './bild_ico/EC4_WebCam_144_mask.png',
     './bild_ico/EC4_WebCam_144.png'];
 
 
 importScripts('./sw-toolbox/sw-toolbox.js');
+
+
 toolbox.precache([
-    './css/normalize.css',
-    './css/BLE_style.css',
-    
-    './sw-toolbox/companion.js',
-    './js/BLE_COM_UI.js',
-    './ble.html',
-    
-    
+  
+    './sw-toolbox/companion.js'
   ]);
 
   toolbox.precache(file_cashe);
